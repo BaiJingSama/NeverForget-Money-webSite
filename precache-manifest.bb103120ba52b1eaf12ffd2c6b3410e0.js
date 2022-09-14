6c3e731e959220c89addf9a5cbbac7c93e09418e
Jackeybai <1303802862@qq.com>
2022-09-14 12:03:50 +0800

    deploy

---
+++ precache-manifest.bb103120ba52b1eaf12ffd2c6b3410e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d0bce9eb6c5efe0d2d0",
+    "url": "/NeverForget-Money-webSite/css/app.709ec17b.css"
+  },
+  {
+    "revision": "79596403b1402dd8d14eaa5098f447eb",
+    "url": "/NeverForget-Money-webSite/index.html"
+  },
+  {
+    "revision": "0d0bce9eb6c5efe0d2d0",
+    "url": "/NeverForget-Money-webSite/js/app.fa9f211f.js"
+  },
+  {
+    "revision": "352018c529f7dcbaab70",
+    "url": "/NeverForget-Money-webSite/js/chunk-vendors.fa29027f.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/NeverForget-Money-webSite/manifest.json"
+  },
+  {
+    "revision": "e4ebb6add4386019ea3e87b86bafac02",
+    "url": "/NeverForget-Money-webSite/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NeverForget-Money-webSite/robots.txt"
+  }
+]);
